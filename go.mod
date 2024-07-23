ed57ffc90e94b5612fd214c039f49effe8012477
cbr <cbr@dips.no>
2024-07-23 10:44:08 +0200

    add ns patch test

--- go.mod
+++ go.mod
@@ -9,7 +9,11 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pkg/helmgen/generate_test.go
@@ -0,0 +1,39 @@
+package helmgen
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	jsonIterator "github.com/json-iterator/go"
+	"github.com/stretchr/testify/assert"
+	"sigs.k8s.io/yaml"
+)
+
+func TestPatchNamespace(t *testing.T) {
+	t.Run("static namespace element gets templated", func(t *testing.T) {
+		data, err := os.ReadFile(filepath.Join("..", "..", "test", "operator-helm-gen", "testdata", "rolebinding.yaml"))
+
+		if assert.NoError(t, err) {
+			assert.NotEmpty(t, data)
+		}
+
+		j, err := yaml.YAMLToJSON(data)
+		if assert.NoError(t, err) {
+			assert.NotEmpty(t, j)
+		}
+
+		resourceNS := jsonIterator.Get(j, "metadata", "namespace").ToString()
+		assert.NotEmpty(t, resourceNS)
+		assert.NotContains(t, resourceNS, ".Release.Namespace")
+
+		patched, err := patchResource(j)
+		if assert.NoError(t, err) {
+			assert.NotEmpty(t, patched)
+		}
+
+		patchedResourceNS := jsonIterator.Get(patched, "metadata", "namespace").ToString()
+		assert.NotEmpty(t, patchedResourceNS)
+		assert.Contains(t, patchedResourceNS, ".Release.Namespace")
+	})
+}
